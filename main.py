3ac00b4d5e092a6dc124e601d70fd4e75bab9a4a
gokultej <38723397+gokultej@users.noreply.github.com>
2023-08-12 12:27:56 +0530

    Adding Credit Card feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas
 
 df = pandas.read_csv("hotels.csv",dtype={"id":str})
+df_cards = pandas.read_csv("cards.csv",dtype=str).to_dict(orient="records")
 
 class Hotel:
     def __init__(self,hotel_id):
@@ -25,22 +26,31 @@ class ReservationTicket:
     def generate(self):
         content =f"""
         Thank you for your reseervation!
-        here are your bookung data:
+        here are your booking data:
         Name:{self.customer_name} 
         Hotel name:-{self.hotel.name}"""
         return content
 
 
 class CreditCard:
-    pass
+    def __init__(self,number):
+        self.number = number
+
+    def validate(self,expiration,holder,cvc):
+        card_data = {"number": self.number,"expiration":expiration,"holder":holder,
+                     "cvc":cvc}
+        if card_data in df_cards:
+            return True
+        else:
+            return False
 print(df)
 hotel_ID = input("Enter the id of the Hotel:")
 hotel = Hotel(hotel_ID)
 
 
 if hotel.available():
-    credit_card = CreditCard(number="1234567890123456",expiration="12/26",holder="JOhn smith",cvc="123")
-    if credit_card.validate():
+    credit_card = CreditCard(number="1234")
+    if credit_card.validate(expiration="12/26",holder="JOHN SMITH",cvc="123"):
         hotel.book()
         name = input("Enter your name:")
         reservation_ticket = ReservationTicket(customer_name=name,hotel_object=hotel)
